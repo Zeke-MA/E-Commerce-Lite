5beef53ab4908061c5c11e0f2435a7db01877028
Zeke-MA <gregorios.tzikas@gmail.com>
2025-01-12 00:41:16 -0500

    RefreshAccessToken function created. Need to generate new token and send back in response

--- internal/handlers/token.go
+++ internal/handlers/token.go
@@ -1,7 +1,29 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Zeke-MA/E-Commerce-Lite/internal/auth"
+	"github.com/Zeke-MA/E-Commerce-Lite/internal/server"
+)
 
 func (cfg *HandlerSiteConfig) RefreshAccessToken(w http.ResponseWriter, r *http.Request) {
 	// if the responding user exists and has a valid access token generate new access token and respond back
+	type TokenResponse struct {
+		AccessToken string `json:"access_token"`
+	}
+
+	bearerToken, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		server.RespondWithError(w, http.StatusUnauthorized, "Access token missing", err)
+		return
+	}
+
+	_, err = auth.ValidateJWT(bearerToken, cfg.JWTSecret)
+
+	if err != nil {
+		server.RespondWithError(w, http.StatusUnauthorized, "Unauthorized token request", err)
+		return
+	}
+
 }
