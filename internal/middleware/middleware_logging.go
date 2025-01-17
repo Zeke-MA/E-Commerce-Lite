748671b2e7fd539aa32c7753ed5bcd17b6457042
Zeke-MA <gregorios.tzikas@gmail.com>
2025-01-17 00:08:11 -0500

    Middleware logging added to all subrouters

--- internal/middleware/middleware_logging.go
+++ internal/middleware/middleware_logging.go
@@ -1,15 +1,61 @@
 package middleware
 
-import "net/http"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"net/http/httputil"
+)
 
-func LogIncomingRequest(next http.Handler) http.Handler {
+func (cfg *MiddlewareSiteConfig) LogIncomingRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Attempt to open the log file
-		// Generate the log file structure
-		// Capture the fields if available
-		// Write to the log
-		// Save the log
-		// defer close the file
-		// serve the http endpoint
+		var i interface{}
+
+		req, err := httputil.DumpRequest(r, false)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		bodyBytes, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			cfg.Logger.Error("Reading Response Body", slog.String("error", fmt.Sprintf("%v", err)))
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		err = json.Unmarshal(bodyBytes, &i)
+
+		if err != nil {
+			cfg.Logger.Error("Parsing Response Body", slog.String("error", fmt.Sprintf("%v", err)))
+		}
+
+		obj, ok := i.(map[string]interface{})
+
+		if !ok {
+			cfg.Logger.Error("Wrong Request Format", slog.String("error", fmt.Sprintf("%v", err)))
+		}
+
+		delete(obj, "password")
+		delete(obj, "access_token")
+		delete(obj, "refresh_token")
+
+		jsonBytes, err := json.Marshal(obj)
+
+		if err != nil {
+			log.Printf("Error marshalling map: %v \n", err)
+		}
+
+		cfg.Logger.Info("Requestor Details", slog.String("details", string(req)))
+		cfg.Logger.Info("Request body", slog.String("request body", string(jsonBytes)))
+
+		next.ServeHTTP(w, r)
 	})
 }
